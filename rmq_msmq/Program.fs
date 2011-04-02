11dff186fcd2bb318c5c64d6493b6120e1493fce
Alvaro Videla <videlalvaro@gmail.com>
2011-04-02 03:14:14 +0200

    project files

---
+++ rmq_msmq/Program.fs
@@ -0,0 +1,14 @@
+﻿module Program
+
+open System.Messaging
+open RabbitMQ.Client
+
+open MSMQ.Util
+open RabbitMQ.Helper
+open RabbitMQ.Bridge
+
+Bridge.msmq2rmq ("guest", "guest", "/", "localhost", 
+            AmqpTcpEndpoint.UseDefaultPort, 
+            Protocols.SafeLookup("AMQP_0_9")) 
+         ("", "ms-test", null)
+         @".\private$\MyTestingQueues"
