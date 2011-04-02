4e492b0bb163da076dda86056945f4726550881f
Alvaro Videla <videlalvaro@gmail.com>
2011-04-02 03:13:09 +0200

    first commit

---
+++ RabbitMQUtils/RabbitMQ.Bridge.Bridge.fs
@@ -0,0 +1,30 @@
+﻿module RabbitMQ.Bridge.Bridge
+
+open System.Messaging
+open RabbitMQ.Client
+
+open MSMQ.Util
+open RabbitMQ.Helper
+
+let onMessage (rmqPublisher:Publisher) (receiver:Receiver)(source:obj) (asyncResult:ReceiveCompletedEventArgs) = 
+    try
+        let queue = source :?> MessageQueue
+        let msg  = queue.EndReceive(asyncResult.AsyncResult)
+        let body = msg.Body.ToString()
+        if body = "quit" then
+            receiver.keepConsuming <- false
+        else
+            rmqPublisher.PublishMsg body
+            queue.BeginReceive()
+            |> ignore
+    with
+        | err -> printfn "Error: %s" err.Message
+
+let msmq2rmq connOptions (exchange, rKey, basicProperties) msQueue = do
+    let f = factory connOptions
+    use conn = f.CreateConnection()
+    use chann = conn.CreateModel()
+    use publisher = new Publisher(chann, exchange, rKey, basicProperties)
+    use q = MSMQ.Util.queue msQueue
+    use receiver = new Receiver(q)
+    receiver.startConsuming q (new BinaryMessageFormatter()) (onMessage publisher receiver)
---
+++ RabbitMQUtils/RabbitMQ.Helper.fs
@@ -0,0 +1,32 @@
+﻿module RabbitMQ.Helper
+
+open RabbitMQ.Client
+
+/// gets a ConnectionFactory
+let factory (user, pass, vhost, hostname, port, protocol) : ConnectionFactory = 
+    let f = new ConnectionFactory()
+    f.UserName    <- user
+    f.Password    <- pass
+    f.VirtualHost <- vhost
+    f.Protocol    <- protocol
+    f.HostName    <- hostname
+    f.Port        <- port
+    f
+
+///Publish MSMQ messages to exchange using rKey
+type Publisher(chann:IModel, exchange:string, rKey:string, basicProperties:IBasicProperties) =
+    let mutable disposed = false
+    
+    let cleanup() =
+        if not disposed then
+            disposed <- true;
+            chann.Dispose();
+
+    interface System.IDisposable with
+        member x.Dispose() = cleanup()
+
+    member this.CloseAll() = cleanup()
+     
+    member this.PublishMsg (body:string) =
+        let content = System.Text.Encoding.UTF8.GetBytes(body)
+        chann.BasicPublish(exchange, rKey, basicProperties, content)
